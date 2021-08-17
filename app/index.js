47abc433403ec799d0e194f2756d91c85fd14181
reyhane javivi <1100.reyhanej@gmail.com>
2021-08-17 20:55:47 +0430

    fixed the start errors

--- app/index.js
+++ app/index.js
@@ -1,25 +1,24 @@
-const express = require("express")
-const morgan = require("morgan")
-const app = express()
-const helmet = require("helmet")
-const rateLimit = require("express-rate-limit")
-const cors = require("cors")
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const cors = require("cors");
 
-const port = config.port || "3000"
-const { mongoConnection } = require("app/helpers/init_mongodb")
-const { config } = require("dotenv")
+const port = config.port || 3000;
+const { mongoConnection } = require("app/helpers/init_mongodb");
 
 module.exports = class Application {
     constructor() {
-        this.runExpressAndMongo()
-        this.setConfigs()
-        this.setRoutes()
+        this.runExpressAndMongo();
+        this.setConfigs();
+        this.setRoutes();
     }
     runExpressAndMongo() {
         app.listen(port, () => {
-            console.log(`server is running on port ${port}`)
-        })
-        mongoConnection()
+            console.log(`server is running on port ${port}`);
+        });
+        mongoConnection();
     }
     setConfigs() {
         const limiter = new rateLimit({
@@ -28,34 +27,34 @@ module.exports = class Application {
             handler: (req, res) => {
                 return res.json({
                     data: "تعداد درخواست های شما بیشتر از حد بوده لطفا بعدا تلاش کنید",
-                    status: "error"
-                })
-            }
-        })
-        app.use(limiter)
-        const whiteList = ["http://localhost:3000"]
+                    status: "error",
+                });
+            },
+        });
+        app.use(limiter);
+        const whiteList = ["http://localhost:3001"];
         if (config.useCorsOptions !== "false") {
             const corsOptions = {
                 origin: function(prigin, callback) {
                     if (whiteList.indexOf(origin) !== -1) {
-                        callback(null, true)
-                        return
+                        callback(null, true);
+                        return;
                     }
-                    callback(new Error("not allowed by CORS"))
-                }
-            }
-            app.use(cors(corsOptions))
+                    callback(new Error("not allowed by CORS"));
+                },
+            };
+            app.use(cors(corsOptions));
         } else {
             app.use(cors());
         }
-        app.use(helmet())
+        app.use(helmet());
         if (config.devMode) {
-            app.use(morgan("dev"))
+            app.use(morgan("dev"));
         }
-        app.use(express.json())
-        app.use(express.urlencoded({ extended: true }))
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
     }
     setRoutes() {
-        app.use(require("app/routes"))
+        app.use(require("app/routes"));
     }
-}
+};
